4259e721eca8cb0ecb21489f47be02c7825df76a
Serhat Tomak <serhattomak@gmail.com>
2024-01-02 14:18:49 +0300

    Modified Calculation Pages Calculation pages have been modified.

--- Controllers/CalculationController.cs
+++ Controllers/CalculationController.cs
@@ -15,6 +15,24 @@ public class CalculationController : Controller
 
     public IActionResult SaturatedWaterTemperature()
     {
+        /*if (model.InputTemperature.HasValue)
+        {
+            var data = GetDataFromDatabase();
+
+            if (data != null)
+            {
+                // Eşleşen verileri güncelleme yeri
+
+                model.SaturationPressure = data.SaturationPressure;
+
+                // Diğer özellikler de burada güncellenebilir.
+            }
+        }
+        else
+        {
+            //Eşleşen veri yoksa interpolasyon.
+            model.SaturationPressure=Interpolasyon(model.InputTemperature.Value);
+        }*/
         return View();
     }
     public IActionResult SaturatedWaterPressure()
